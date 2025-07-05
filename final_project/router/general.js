2bb0144195c6357d4fa981264ac41f5db3ddb27f
myzra <myzrahfortnite@gmail.com>
2025-07-06 01:05:02 +0200

    task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  b = JSON.stringify(books)
-  return res.status(200).json(books);
+  const b = JSON.stringify(books)
+  return res.status(200).send(b);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn =  req.params.isbn;
+  const b = books[isbn];
+  
+  if(b) {
+    return res.status(200).json(b);
+  } else {
+    return res.status(404).json({ message: "Book not found"});
+  }
  });
   
 // Get book details based on author
