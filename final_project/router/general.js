2cd1e74fa24a306747a70689c84ff6305006b608
myzra <myzrahfortnite@gmail.com>
2025-07-06 01:17:48 +0200

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,8 +44,15 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const t = req.params.title;
+  const bArray = Object.values(books);
+  const b = bArray.filter(book => book.title === t);
+  
+  if(b) {
+    return res.status(200).json(b);
+  } else {
+    return res.status(404).json({ message: "No books found"})
+  }
 });
 
 //  Get book review
